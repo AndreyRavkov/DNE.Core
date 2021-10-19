c6febe79e0700d9ccb36fcd290270b05366a186d
andrey <andreyravkov@gmail.com>
2021-10-19 13:17:43 +0300

    add collection and string Extensions

---
+++ src/DNE.Core/Collection/Extensions.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+namespace DNE.Core.Collection
+{
+    public static class Extensions
+    {
+        public static IReadOnlyList<T> ToList<T>(this T value)
+        {
+            if (value == null)
+            {
+                return new List<T>();
+            }
+
+            return new List<T>() {value};
+        }
+        
+        public static IEnumerable<T> ToEnumerable<T>(this T value)
+        {
+            return ToList(value);
+        }
+
+        public static IReadOnlyList<T> ToReadOnlyList<T>(this T value)
+        {
+            return ToList(value);
+        }
+    }
+}
--- src/DNE.Core/String/Extensions.cs
+++ src/DNE.Core/String/Extensions.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 namespace DNE.Core.String
 {
     public static class Extensions
@@ -13,6 +18,62 @@ namespace DNE.Core.String
         {
             return string.IsNullOrEmpty(value) ? value : value.Trim();
         }
+
+        /// <summary>
+        /// Replace keys to value in the current string
+        /// </summary>
+        /// <param name="value">original string with keys</param>
+        /// <param name="replaceDictionary">dictionary with key-value</param>
+        /// <returns>string</returns>
+        public static string Replace(this string value, Dictionary<string, string> replaceDictionary)
+        {
+            if (!replaceDictionary.Any())
+            {
+                return value;
+            }
+            foreach (var item in replaceDictionary)
+            {
+                value = value.Replace(item.Key, item.Value);
+            }
+
+            return value;
+        }
+        
+        /// <summary>
+        /// Convert string to Base64 string
+        /// </summary>
+        /// <param name="text">string value</param>
+        /// <param name="encoding">encoding type, if leave this param as null, the method will use System.Text.Encoding.UTF8</param>
+        /// <returns>string</returns>
+        public static string ToBase64(this string text, Encoding encoding = null)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return text;
+            }
+            encoding ??= Encoding.UTF8;
+
+            byte[] textAsBytes = encoding.GetBytes(text);
+            return Convert.ToBase64String(textAsBytes);
+        }
+
+        /// <summary>
+        /// Convert base64 string
+        /// </summary>
+        /// <param name="text">string value</param>
+        /// <param name="encoding">encoding type, if leave this param as null, the method will use System.Text.Encoding.UTF8</param>
+        /// <returns></returns>
+        public static string FromBase64(this string text, Encoding encoding = null)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return text;
+            }
+            encoding ??= Encoding.UTF8;
+            byte[] textAsBytes = Convert.FromBase64String(text);
+            return encoding.GetString(textAsBytes);
+        }
+        
         
         
     }
